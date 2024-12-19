56993e7169e341d7164383d12cd0635727b3082f
Vass Norbert <vassnorbert20@gmail.com>
2024-12-19 10:00:52 +0100

    Day 4: Solution part two

--- day_4.py
+++ day_4.py
@@ -1,8 +1,8 @@
 # https://adventofcode.com/2024/day/4
 INPUTS = "inputs/day_4.txt"
-SEARCH_WORD = "XMAS"
 
 def one_star():
+    SEARCH_WORD = "XMAS"
     with open(INPUTS, "r") as file:
         data = [line.strip() for line in file]
 
@@ -25,7 +25,23 @@ def one_star():
     return total_words
 
 def two_star():
-    pass
+    SEARCH_WORD = "MAS"
+    with open(INPUTS, "r") as file:
+        data = [line.strip() for line in file]
+
+    total_words = 0
+    directions = [(-1, -1, 1, 1), (-1, 1, 1, -1), (1, -1, -1, 1), (1, 1, -1, -1)]
+
+    def search(i, j):
+        words = [data[i + s1][j + s2] + 'A' + data[i + e1][j + e2] for s1, s2, e1, e2 in directions]
+        return words.count(SEARCH_WORD) == 2
+
+    for i in range(1, len(data) - 1):
+        for j in range(1, len(data[0]) - 1):
+            if data[i][j] == 'A' and search(i, j):
+                total_words += 1
+    
+    return total_words
 
 if __name__ == "__main__":
     print("One star solution:", one_star())
